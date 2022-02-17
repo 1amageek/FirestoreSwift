119d276a589c2f8f22de8d0e28c726858b130f38
1amageek <tmy0x3@icloud.com>
2022-02-17 12:14:08 +0900

    Fix

--- Tests/FirestoreSwiftTests/FirestoreSwiftTests.swift
+++ Tests/FirestoreSwiftTests/FirestoreSwiftTests.swift
@@ -1,11 +1,33 @@
 import XCTest
+import FirebaseFirestore
+import DocumentID
 @testable import FirestoreSwift
 
 final class FirestoreSwiftTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(FirestoreSwift().text, "Hello, World!")
+
+    func testCodable() throws {
+
+        struct NestData: Identifiable, Codable {
+            @DocumentID var id: String
+            var number: Int
+        }
+
+        struct TopData: Identifiable, Codable {
+            @DocumentID var id: String
+            var number: Int
+            var nest: NestData
+        }
+        
+        let nestData = NestData(id: "0", number: 0)
+        let topData = TopData(id: "0", number: 0, nest: nestData)
+
+        let encodedNest = try? Firestore.Encoder().encode(nestData)
+        let encodedTop = try? Firestore.Encoder().encode(topData)
+
+        print(encodedNest)
+        print(encodedTop)
+
+//        Firestore.Decoder().decode(<#T##T#>, from: <#T##[String : Any]#>)
+
     }
 }
