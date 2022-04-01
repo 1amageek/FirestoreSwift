5666f131491f2c3192b838226442d3e977a2308d
1amageek <tmy0x3@icloud.com>
2022-04-01 18:32:16 +0900

    Fix

--- Sources/FirestoreSwift/Concurrency/Query+Concurrency.swift
+++ Sources/FirestoreSwift/Concurrency/Query+Concurrency.swift
@@ -71,6 +71,46 @@ extension Query {
         }
     }
 
+    public func changes<T>(type: T.Type, includeMetadataChanges: Bool = false) -> AsyncThrowingStream<(added: [T], modified: [T], removed: [T]), Error> where T: Decodable {
+        AsyncThrowingStream { continuation in
+            let listener = self.addSnapshotListener(includeMetadataChanges: includeMetadataChanges) { querySnapshot, error in
+                if let error = error {
+                    continuation.finish(throwing: error)
+                    return
+                }
+                do {
+                    let changes = querySnapshot?.documentChanges(includeMetadataChanges: includeMetadataChanges) ?? []
+                    var added: [T] = []
+                    var modified: [T] = []
+                    var removed: [T] = []
+                    try changes.forEach { documentChange in
+                        switch documentChange.type {
+                            case .added:
+                                if let document = try documentChange.document.data(as: type, with: .estimate) {
+                                    added.append(document)
+                                }
+                            case .modified:
+                                if let document = try documentChange.document.data(as: type, with: .estimate) {
+                                    modified.append(document)
+                                }
+                            case .removed:
+                                if let document = try documentChange.document.data(as: type, with: .estimate) {
+                                    removed.append(document)
+                                }
+                        }
+                    }
+                    continuation.yield((added, modified, removed))
+                } catch {
+                    print(#function, #line, error)
+                    continuation.finish(throwing: error)
+                }
+            }
+            continuation.onTermination = { @Sendable _ in
+                listener.remove()
+            }
+        }
+    }
+
     public func changes<T>(type: T.Type, includeMetadataChanges: Bool = false) -> AsyncThrowingStream<((added: [T], modified: [T], removed: [T]), QuerySnapshot), Error> where T: Decodable {
         AsyncThrowingStream { continuation in
             let listener = self.addSnapshotListener(includeMetadataChanges: includeMetadataChanges) { querySnapshot, error in
