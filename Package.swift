8f5aaaced84c94c180868131b6835e842721a66b
1amageek <nori@stamp.team>
2022-10-12 18:15:10 +0900

    Fix

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "git@github.com:1amageek/DocumentID.git", .branch("main")),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "9.1.0"))
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "10.0.0"))
     ],
     targets: [
         .target(
